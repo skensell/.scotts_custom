600c6da58ac88723ed5ce20fa9197dc4e166df35
Scott Kensell <skensell@gmail.com>
2015-08-05 00:55:26 +0000

    Added bootstrap.sh file.

--- README.md
+++ README.md
@@ -6,10 +6,6 @@ My bash and vim customizations.
 #### Quick Setup
 
 ```bash
-    cd ~
-    git clone git@github.com:skensell/.scotts_custom.git 
-    ln -s ~/.scotts_custom/bash_profile/.bash_profile ~/.bash_profile
-    ln -s ~/.scotts_custom/vim/.vimrc ~/.vimrc
-    ln -s ~/.scotts_custom/bin ~/bin
+    ./bootstrap.sh
 ```
 
---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+function link_or_warn(){
+    local f=$(basename "$1")
+    if [ -e "$HOME/$f" ]; then
+        echo >&2 "$HOME/$f exists already. Skipping linking phase."
+    else
+        ln -sv "$1" "$HOME/$f"
+    fi
+}
+
+cd "$HOME"
+if [ -d '.scotts_custom' ]; then
+    echo ".scotts_custom exists already, skipping cloning stage."
+else
+    git clone git@github.com:skensell/.scotts_custom.git 
+fi
+    
+
+link_or_warn "$HOME/.scotts_custom/bash_profile/.bash_profile"
+link_or_warn "$HOME/.scotts_custom/bin"
+link_or_warn "$HOME/.scotts_custom/vim/.vimrc"
+
+if [ ! -e "$HOME/.vim/bundle/vundle" ]; then
+    git clone git://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
+
+echo "Bootstrap successful. If you want to finish the vim setup, run: vim +BundleInstall +qall"
