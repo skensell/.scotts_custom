6a0b5cd785b9bcd50257dc2487c43e74d94a2fef
Scott Kensell <skensell@gmail.com>
2015-08-06 19:24:56 +0200

    Update ratios from uniq to work on Python 2.6-.

--- bin/pybin/ratios_from_uniq.py
+++ bin/pybin/ratios_from_uniq.py
@@ -9,4 +9,4 @@ ints = [ int(line.split()[0]) for line in lines ]
 s = sum(ints)
 ratios = [ x/float(s) for x in ints ]
 for i in range(len(lines)):
-    print "{:>6.2%} {!s}".format(ratios[i], lines[i].rstrip())
+    print "%6.2f%% %s"%(100*ratios[i], lines[i].rstrip())
